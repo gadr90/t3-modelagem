a6d384f3b872a49ea0e9022281ded497d0ef8e9b
Guilherme Rodrigues <gadr90@gmail.com>
2012-11-11 19:46:56 -0200

    Grid de debug

--- src/inf/pucrio/modelagem/t3/gui/MonopolyFrame.java
+++ src/inf/pucrio/modelagem/t3/gui/MonopolyFrame.java
@@ -5,6 +5,8 @@ import inf.pucrio.modelagem.t3.Player;
 import inf.pucrio.modelagem.t3.utils.PositionUtils;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
@@ -24,7 +26,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MonopolyFrame extends JFrame implements Observer {
-	
+
 	/**
 	 * 
 	 */
@@ -34,44 +36,53 @@ public class MonopolyFrame extends JFrame implements Observer {
 	private JLabel roll2;
 	private JLabel player;
 	private Game game;
-	
+
 	public MonopolyFrame(Game game) {
 		super("0921720;0920523");
 		this.game = game;
 		this.game.addObserver(this);
 		System.out.println("Initializing MonopolyFrame...");
-		//Graphic initialization
-		this.setSize(PositionUtils.BOARD_SIZE_PIXELS + 200, PositionUtils.BOARD_SIZE_PIXELS);
+		// Graphic initialization
+		this.setSize(PositionUtils.BOARD_SIZE_PIXELS + 230,
+				PositionUtils.BOARD_SIZE_PIXELS + 50);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		diceButton = new JButton("Jogar Dados");
 		diceButton.addMouseListener(new RollDiceButtonMouseListener());
-		
+
 		roll1 = new JLabel("Dado 1: ");
 		roll2 = new JLabel("Dado 2: ");
-		player = new JLabel("Jogador: " + game.getCurrentPlayer().getPlayerName());
-		
-		/*Font font = new Font(Font.MONOSPACED, Font.BOLD, 30);
-		hours.setFont(font);
-		minutes.setFont(font);
-		colon.setFont(font);
-		*/
-		
+		player = new JLabel("Jogador: "
+				+ game.getCurrentPlayer().getPlayerName());
+
+		/*
+		 * Font font = new Font(Font.MONOSPACED, Font.BOLD, 30);
+		 * hours.setFont(font); minutes.setFont(font); colon.setFont(font);
+		 */
+
 		JPanel centerPanel = new JPanel(null);
+
 		JPanel eastPanel = new JPanel();
 		eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
-		
-		//Adiciona todos os players no tabuleiro representado pelo Panel
+
+		// Adiciona todos os players no tabuleiro representado pelo Panel
 		for (Player p : getGame().getPlayers()) {
 			centerPanel.add(p.getView());
 			p.getView().repaint();
 		}
-		
-		//Desenhando tabuleiro
+
+		// Desenhando tabuleiro
 		try {
-			Image image = ImageIO.read(new File("bancoimobiliario1.png")).getScaledInstance(650, 650, java.awt.Image.SCALE_SMOOTH);
+			Image image = ImageIO.read(new File("bancoimobiliario1.png"))
+					.getScaledInstance(650, 650, java.awt.Image.SCALE_SMOOTH);
 			ImageIcon icon = new ImageIcon(image);
-			JLabel picLabel = new JLabel(icon);
+			JLabel picLabel = new JLabel(icon) {
+				public void paintComponent(Graphics g) {
+					super.paintComponent(g);
+					Graphics2D g2d = (Graphics2D) g;
+					PositionUtils.drawDebugLines(this, g2d, false);
+				}
+			};
 			picLabel.setSize(650, 650);
 			picLabel.setVisible(true);
 			centerPanel.add(picLabel);
@@ -79,16 +90,16 @@ public class MonopolyFrame extends JFrame implements Observer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		/*
-		centerPanel.add(minutes, BoxLayout.X_AXIS);
-		centerPanel.add(colon, BoxLayout.X_AXIS);
-		centerPanel.add(hours, BoxLayout.X_AXIS);
+		 * centerPanel.add(minutes, BoxLayout.X_AXIS); centerPanel.add(colon,
+		 * BoxLayout.X_AXIS); centerPanel.add(hours, BoxLayout.X_AXIS);
 		 */
-		
-		centerPanel.setSize(PositionUtils.BOARD_SIZE_PIXELS, PositionUtils.BOARD_SIZE_PIXELS);
+
+		centerPanel.setSize(PositionUtils.BOARD_SIZE_PIXELS,
+				PositionUtils.BOARD_SIZE_PIXELS);
 		eastPanel.setSize(200, PositionUtils.BOARD_SIZE_PIXELS);
-		
+
 		this.getContentPane().add(centerPanel, BorderLayout.CENTER);
 		this.getContentPane().add(eastPanel, BorderLayout.EAST);
 
@@ -96,7 +107,7 @@ public class MonopolyFrame extends JFrame implements Observer {
 		eastPanel.add(roll1);
 		eastPanel.add(roll2);
 		eastPanel.add(player);
-		
+
 		this.setVisible(true);
 		System.out.println("Finished Initializing MonopolyFrame.");
 	}
@@ -106,33 +117,36 @@ public class MonopolyFrame extends JFrame implements Observer {
 		System.out.println("Notified of update!");
 		Game game = this.getGame();
 		Player lastPlayer = game.getLastPlayer();
-		Point p = PositionUtils.getPositionForIndex(lastPlayer.getCurrentIndex(), game.getPlayers().indexOf(lastPlayer));
+		Point p = PositionUtils.getPositionForIndex(
+				lastPlayer.getCurrentIndex(),
+				game.getPlayers().indexOf(lastPlayer));
 		lastPlayer.getView().setBounds(p.x, p.y, 20, 20);
-//		currentPlayer.getView().repaint();
+		// currentPlayer.getView().repaint();
 		roll1.setText("Dado 1: " + String.valueOf(game.getCurrentRoll1()));
 		roll2.setText("Dado 2: " + String.valueOf(game.getCurrentRoll2()));
 		player.setText("Jogador: " + game.getCurrentPlayer().getPlayerName());
 	}
-	
+
 	public Game getGame() {
 		return this.game;
 	}
 
 	class RollDiceButtonMouseListener extends MouseAdapter {
 		private Game getGame(MouseEvent e) {
-			return ((MonopolyFrame) ((JComponent) e.getComponent()).getTopLevelAncestor()).getGame();
+			return ((MonopolyFrame) ((JComponent) e.getComponent())
+					.getTopLevelAncestor()).getGame();
 		}
-		
+
 		@Override
 		public void mousePressed(MouseEvent e) {
 			System.out.println("Pressed roll dice button");
 			getGame(e).doTurn();
 		}
-		
+
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			System.out.println("Released button A");
 		}
 	}
-	
+
 }
--- src/inf/pucrio/modelagem/t3/utils/PositionUtils.java
+++ src/inf/pucrio/modelagem/t3/utils/PositionUtils.java
@@ -1,72 +1,107 @@
 package inf.pucrio.modelagem.t3.utils;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Stroke;
+
+import javax.swing.JComponent;
 
 public class PositionUtils {
-	
+
 	public static final int BOARD_TILES = 13;
 	public static final int BOARD_SIZE_PIXELS = 650;
-	public static final int TILE_SIZE_PIXELS = BOARD_SIZE_PIXELS/BOARD_TILES;
-	
+	public static final int TILE_SIZE_PIXELS = BOARD_SIZE_PIXELS / BOARD_TILES;
+
 	/**
 	 * Devolve as coordernadas X,Y de um player dentro de um tile.
-	 * @param index índice do jogador
-	 * @param tileWidth Número de tiles horizontais
+	 * 
+	 * @param index
+	 *            índice do jogador
+	 * @param tileWidth
+	 *            Número de tiles horizontais
 	 */
 	public static Point playerCoordsInTile(int index, int tileWidth) {
 		return new Point(index % tileWidth, (int) Math.floor(index / tileWidth));
 	}
-	
+
 	public static Point tileCoordsInBoard(int index) {
 		if (index > 39) {
 			index -= 40;
 		}
-		if (index == 0) return new Point(11, 11);
-		if (index == 10) return new Point(0, 11);
-		if (index == 20) return new Point(0, 0);
-		if (index == 30) return new Point(11, 0);
-		
-		if (index < 10) return new Point((11 - index), 11);			
-		if (index < 20) return new Point(0, 11 - (index - 10));
-		if (index < 30) return new Point((index - 21) + 2, 0);
-		return new Point(11, (index - 31) + 2);	
+		if (index == 0)
+			return new Point(11, 11);
+		if (index == 10)
+			return new Point(0, 11);
+		if (index == 20)
+			return new Point(0, 0);
+		if (index == 30)
+			return new Point(11, 0);
+
+		if (index < 10)
+			return new Point((11 - index), 11);
+		if (index < 20)
+			return new Point(0, 11 - (index - 10));
+		if (index < 30)
+			return new Point((index - 21) + 2, 0);
+		return new Point(11, (index - 31) + 2);
 	}
-	
+
 	public static Point relativeToGlobal(Point p) {
 		return new Point(p.x * TILE_SIZE_PIXELS, p.y * TILE_SIZE_PIXELS);
 	}
-	
+
 	public static Point tileRelativeToGlobal(Point p) {
-		return new Point(p.x * TILE_SIZE_PIXELS/2, p.y * TILE_SIZE_PIXELS/2);			
+		return new Point(p.x * TILE_SIZE_PIXELS / 2, p.y * TILE_SIZE_PIXELS / 2);
 	}
-	
+
 	public static Point getPositionForIndex(int boardIndex, int playerIndex) {
 		Point tilePosition = PositionUtils.tileCoordsInBoard(boardIndex);
-		Point positionInTile = PositionUtils.playerCoordsInTile(playerIndex, PositionUtils.getWidthForTile(boardIndex));
+		Point positionInTile = PositionUtils.playerCoordsInTile(playerIndex,
+				PositionUtils.getWidthForTile(boardIndex));
 		Point globalTilePosition = PositionUtils.relativeToGlobal(tilePosition);
-		Point globalPositionInTile = PositionUtils.tileRelativeToGlobal(positionInTile);
+		Point globalPositionInTile = PositionUtils
+				.tileRelativeToGlobal(positionInTile);
 		int x = (int) (globalPositionInTile.getX() + globalTilePosition.getX());
 		int y = (int) (globalPositionInTile.getY() + globalTilePosition.getY());
-		
-		System.out.println("Gerando posição para boardIndex: " + boardIndex );
+
+		System.out.println("Gerando posição para boardIndex: " + boardIndex);
 		System.out.println("Tile position" + tilePosition);
 		System.out.println("Position in Tile " + positionInTile);
 		System.out.println("Global tile position" + globalTilePosition);
 		System.out.println("Global position in tile" + globalPositionInTile);
 		System.out.println("Posicionando em " + x + ", " + y);
-		return new Point(x,y);
+		return new Point(x, y);
 	}
 
 	public static int getWidthForTile(int index) {
 		if (index > 39) {
 			index -= 40;
 		}
-		
-		if ( (index > 0 && index < 10) || (index > 20 && index < 30)) {
-			return 2;			
-		}
-		else {
+
+		if ((index > 0 && index < 10) || (index > 20 && index < 30)) {
+			return 2;
+		} else {
 			return 3;
 		}
+	}
+
+	public static void drawDebugLines(JComponent jComponent, Graphics2D g2d, boolean clear) {
+		g2d.setColor(Color.orange);
+		g2d.setStroke(new BasicStroke(2));
+        
+        Dimension size = jComponent.getSize();
+        Insets insets = jComponent.getInsets();
+        int w = size.width - insets.left - insets.right;
+        int h = size.height - insets.top - insets.bottom;
+         
+        for(int i=0; i<w; i=i+TILE_SIZE_PIXELS)
+        {
+            g2d.drawLine(0, i, BOARD_SIZE_PIXELS, i);
+            g2d.drawLine(i, 0, i, BOARD_SIZE_PIXELS);
+        }
 	}
 }
